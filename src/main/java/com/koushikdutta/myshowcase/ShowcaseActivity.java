b2f459fab5b160c05031f13b89d9b65ee0a3932f
Koushik Dutta <koushd@gmail.com>
2019-08-14 19:06:08 -0700

    adnroidx

--- src/main/java/com/koushikdutta/myshowcase/ShowcaseActivity.java
+++ src/main/java/com/koushikdutta/myshowcase/ShowcaseActivity.java
@@ -4,16 +4,17 @@ import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.koushikdutta.boilerplate.WindowChromeCompatActivity;
 
